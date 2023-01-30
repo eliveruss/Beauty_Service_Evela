66cffe58e68d3801976e70e2f14922fdc8c0c907
Aleksander <33355218+eliveruss@users.noreply.github.com>
2023-01-30 01:28:35 +0100

    add swiper(will be changed in future), footer, nav

--- script.js
+++ script.js
@@ -2,11 +2,30 @@ const burgerBtn = document.querySelector('.burger');
 const burgerNav = document.querySelector('.navigation');
 const burgerScrollOff = document.querySelector('body');
 
-function burgerActive (){
+function burgerActive() {
     burgerBtn.classList.toggle('active');
     burgerNav.classList.toggle('active');
     burgerScrollOff.classList.toggle('active');
 }
 
 burgerBtn.addEventListener('click', burgerActive);
-    
+
+
+
+const swiper = new Swiper('.swiper', {
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
