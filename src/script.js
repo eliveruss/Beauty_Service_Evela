bdd66d7473d0912d06b2c566569d9eeceffccf66
Aleksander <33355218+eliveruss@users.noreply.github.com>
2023-02-14 02:51:39 +0100

    add npm sass compiler

--- src/script.js
+++ src/script.js
@@ -14,18 +14,8 @@ burgerBtn.addEventListener('click', burgerActive);
 
 const swiper = new Swiper('.swiper', {
     autoplay: {
-        delay: 3000,
+        delay: 2000,
         disableOnInteraction: false,
     },
     loop: true,
-
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
 });
