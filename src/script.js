a0b674f931bbeaa993325ff2283ba89508ce7769
Olek <aleksander.galdyn@o2.pl>
2023-04-06 08:44:42 +0200

    appearing cosmetics on main page

--- src/script.js
+++ src/script.js
@@ -19,3 +19,16 @@ const swiper = new Swiper('.swiper', {
     },
     loop: true,
 });
+
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
